73a6c6942b15d423112c3e9e88ad6c5ace4d66b9
Christian <christianalva96@gmail.com>
2019-03-17 15:38:39 -0600

    Comments

--- ToDoListApi/Services/TodoRepo.cs
+++ ToDoListApi/Services/TodoRepo.cs
@@ -26,31 +26,29 @@ namespace TareaApi.Services
         {
             get { return _context.TodoItems.ToList(); }
         }
-
+        // Dectecta que los items existan en la Base de Datos
         public bool DoesItemExist(string id)
         {
             return _context.TodoItems.Any(item => item.ID == id);
         }
-
+        // Busca los Items por id a la Base de Datos
         public TodoItem Find(string id)
         {
             return _context.TodoItems.FirstOrDefault(item => item.ID == id);
         }
-
+        // Agrega los Items a la Base de Datos
         public async void Insert(TodoItem item)
         {
             _context.TodoItems.Add(item);
             await _context.SaveChangesAsync();
         }
-
+        // Actualiza un Items por ID a la Base de Datos
         public async void Update(TodoItem item)
         {
-
-                Debug.WriteLine("Hello World");
-                _context.TodoItems.Update(item);
+            _context.TodoItems.Update(item);
             await _context.SaveChangesAsync();
         }
-
+        // Actualiza un Items por ID a la Base de Datos
         public async void Delete(string id)
         {
             _context.TodoItems.Remove(this.Find(id));
